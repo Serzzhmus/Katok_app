afcf42394287fad7cd64fe93770cd73799f097c4
Serzzhmus <98115740+Serzzhmus@users.noreply.github.com>
2023-04-19 00:19:32 +0600

    remote

--- Text.py
+++
@@ -1,8 +0,0 @@
-category_text = "Укажите тариф: "
-time_text = "Время сеанса: "
-konki_text = "Аренда коньков: "
-choice_category_text = ["-", "Полный", "Студенческий", "Пенсионный"]
-choice_time_text = ["-", "1 час", "2 часа", "3 часа"]
-choice_konki_text = ["-", "Да", "Нет"]
-button_text = "Рассчитать"
-price_text = "К оплате: "
--- create_exe.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\Serzh\Desktop\Prokat\Prokat_v02
-C:\Users\Serzh\AppData\Local\Programs\Python\Python39\python.exe freeze.py bdist_msi
-
-
